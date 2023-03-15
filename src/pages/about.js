df874b308645c4036f9a54e34c823ba1e0d3c18d
Deniss1988 <105513759+Deniss1988@users.noreply.github.com>
2023-03-15 08:37:36 +0200

    gatsby website

---
+++ src/pages/about.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function About() {
+  return (
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+          exercitationem soluta nesciunt iste necessitatibus eius quibusdam
+          asperiores officiis praesentium laborum.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+          exercitationem soluta nesciunt iste necessitatibus eius quibusdam
+          asperiores officiis praesentium laborum.
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+          exercitationem soluta nesciunt iste necessitatibus eius quibusdam
+          asperiores officiis praesentium laborum.
+        </p>
+      </div>
+    </Layout>
+  );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
-import * as React from "react"
+import { Link } from "gatsby";
+import * as React from "react";
+import Layout from "../components/Layout";
+import * as styles from "../styles/home.module.css";
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer based in Latvia</p>
+          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
+        </div>
+      </section>
+    </Layout>
+  );
 }
