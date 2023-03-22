32d4fe03d812eba6f66ef851a357bdd1488a4c93
Deniss1988 <105513759+Deniss1988@users.noreply.github.com>
2023-03-22 12:08:25 +0200

    added alt attribute to GatsbyImage

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -18,7 +18,7 @@ export default function Projects({ data }) {
           {projects.map((project) => (
             <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
               <div className={styles.project}>
-                <GatsbyImage image={getImage(project.frontmatter.thumb.childImageSharp.gatsbyImageData)}/>
+                <GatsbyImage image={getImage(project.frontmatter.thumb.childImageSharp.gatsbyImageData)} alt={project.frontmatter.title}/>
                 <h3>{project.frontmatter.title}</h3>
                 <p>{project.frontmatter.stack}</p>
               </div>
