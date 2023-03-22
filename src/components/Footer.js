cbe7801062be484e3bdc80947e54d986ae860643
Deniss1988 <105513759+Deniss1988@users.noreply.github.com>
2023-03-22 12:31:24 +0200

    added rel="noreferrer" attribute

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,32 @@ export default function Footer() {
   return (
     <footer>
       <div className="icon-wrap flex row">
-        <a href="https://www.linkedin.com/in/deniss-mijusovs-11514290/">
+        <a
+          href="https://www.linkedin.com/in/deniss-mijusovs-11514290/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="LinkedIn"
+        >
           <div className="flex icon" id="icon-2">
             <i className="mdi mdi-linkedin"></i>
           </div>
         </a>
-        <a href="https://www.facebook.com/denis.miyusov/">
+        <a
+          href="https://www.facebook.com/denis.miyusov/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Facebook"
+        >
           <div className="flex icon" id="icon-3">
             <i className="mdi mdi-facebook"></i>
           </div>
         </a>
-        <a href="https://www.instagram.com/deniscj1988/">
+        <a
+          href="https://www.instagram.com/deniscj1988/"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="Instagram"
+        >
           <div className="flex icon" id="icon-4">
             <i className="mdi mdi-instagram"></i>
           </div>
